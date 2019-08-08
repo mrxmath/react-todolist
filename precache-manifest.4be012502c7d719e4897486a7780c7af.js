ba89ce5d10c5c08a576b435560cdb5d0aa0e146e
Mateus Oliveira <mateus.oliveira@wunderman.com>
2019-08-08 10:52:45 -0300

    Updates

---
+++ precache-manifest.4be012502c7d719e4897486a7780c7af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b62ce55ecddd902e5f45385843203322",
+    "url": "/mrxmath/react-todolist/index.html"
+  },
+  {
+    "revision": "97e7e712453e7a8da9e8",
+    "url": "/mrxmath/react-todolist/static/css/main.65a25ba5.chunk.css"
+  },
+  {
+    "revision": "e3e1726d43da044c5a6a",
+    "url": "/mrxmath/react-todolist/static/js/2.7cddf10a.chunk.js"
+  },
+  {
+    "revision": "97e7e712453e7a8da9e8",
+    "url": "/mrxmath/react-todolist/static/js/main.40a19510.chunk.js"
+  },
+  {
+    "revision": "f66f64ff8ef6dcee93ab",
+    "url": "/mrxmath/react-todolist/static/js/runtime~main.edaad976.js"
+  }
+]);
